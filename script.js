9005fc333bd337b05d487ce56618a5bb11f4d985
Samuel Ragland <samragland012@gmail.com>
2024-01-17 16:04:36 -0600

    Added Spline 3D animation

--- script.js
+++ script.js
@@ -6,6 +6,15 @@ open.addEventListener('click', () => {
     test = !test;
     test ? open.textContent = '=' : open.textContent = 'x';
   console.log('clicked');
+  console.log(host.shadowRoot);
+  console.log(sheet);
 });
 
-console.log('open');
+const host = document.querySelector( 'spline-viewer' );
+
+const sheet = new CSSStyleSheet;
+host.shadowRoot.lastChild.style.display = 'flex';
+host.shadowRoot.lastChild.style.flexDirection = 'column';
+host.shadowRoot.lastChild.style.justifyContent = 'center';
+host.shadowRoot.lastChild.querySelector('canvas').style.margin = 'auto';
+host.shadowRoot.lastChild.querySelector('a').style.opacity = '0';
